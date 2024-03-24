8c7ec2c0dbb08bd52aab0abde0eea3e5c8e2dc34
wasaw <merenkovalexander@gmail.com>
2024-03-24 11:01:17 +0300

    init

---
+++ Modeling/App/SceneDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  SceneDelegate.swift
+//  Modeling
+//
+//  Created by Александр Меренков on 24.03.2024.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let scene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: scene)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+    }
+
+}
+
---
+++ Modeling/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Modeling
+//
+//  Created by Александр Меренков on 24.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
